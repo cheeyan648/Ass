87cec963d41423beced09c72afeef16fa6a6794a
cheeyan648 <wongcy-wm24@student.tarc.edu.my>
2025-12-19 22:57:26 +0800

    333

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
 $host = "food-db.c1ay6ksyw9rj.us-east-1.rds.amazonaws.com";        // or RDS endpoint
-$dbname = "food-db";        // your database name
+$dbname = "food_db";        // your database name
 $username = "admin";         // DB username
 $password = "food1234";             // DB password
 

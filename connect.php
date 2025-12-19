a9aed40d3dcb79a0ab708b3501d63df8635bfe4a
cheeyan648 <wongcy-wm24@student.tarc.edu.my>
2025-12-19 22:31:03 +0800

    222

--- connect.php
+++ connect.php
@@ -5,14 +5,14 @@ $username = "admin";         // DB username
 $password = "food1234";             // DB password
 
 try {
-    $conn = new PDO(
+    $_db = new PDO(
         "mysql:host=$host;dbname=$dbname;charset=utf8mb4",
         $username,
         $password
     );
 
     // Enable error reporting
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $_db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());

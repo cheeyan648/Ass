056d2fd7e691d7415ea512aa68c7c12934fb4b55
cheeyan648 <wongcy-wm24@student.tarc.edu.my>
2025-12-19 22:03:24 +0800

    first commit

---
+++ admin/admin.php
@@ -0,0 +1,31 @@
+<?php
+
+require '../_base.php';
+
+include '../admin/layout.php';
+
+auth('Admin');
+
+?>
+
+<link rel="stylesheet" href="../css/admin.css">
+
+<div class="layout">
+    <table>
+        <tr>
+            <th>
+                <h1><a href="/admin/product.php">Product</a></h1>
+            </th>
+            <th>
+                <h1><a href="/admin/order.php">Order</a></h1>
+            </th>
+            <th>
+                <h1><a href="/admin/member.php">Member</a></h1>
+            </th>
+            <th>
+                <h1><a href="/admin/setting.php">Setting</a></h1>
+            </th>
+        <tr>
+    </table>    
+</div>
+

74c1f87d18e43f70b31938862f9a4a89daab525d
cheeyan648 <wongcy-wm24@student.tarc.edu.my>
2025-12-21 00:26:13 +0800

    19

--- index.php
+++ index.php
@@ -50,7 +50,7 @@ ini_set('display_startup_errors', 1);
 </style>
 
 <div class="zero_img">
-    <img src="/icon/zero.png" alt="Our zero image" style="height: 80%;">
+    <img src="https://wallpapers.com/best-food-background" alt="Our zero image" style="height: 80%;">
 </div>
 
 <section class="top_seller">

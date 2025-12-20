8d2329ecd810da863eee23b67d0bab7fedd92a21
cheeyan648 <wongcy-wm24@student.tarc.edu.my>
2025-12-20 23:02:50 +0800

    2

--- connect.php
+++ connect.php
@@ -1,7 +1,7 @@
 <?php
 
 try {
-    $db = new PDO("mysql:host=fooddb.c1ay6ksyw9rj.us-east-1.rds.amazonaws.com;port=3306;dbname=fooddb;charset=utf8mb4","admin","food1234");
+    $_db = new PDO("mysql:host=fooddb.c1ay6ksyw9rj.us-east-1.rds.amazonaws.com;port=3306;dbname=fooddb;charset=utf8mb4","admin","food1234");
     echo "Connected!";
 } catch (PDOException $e) {
     echo "Failed: ".$e->getMessage();

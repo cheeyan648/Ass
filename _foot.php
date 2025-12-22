f002e4f28e1f7e8bba4ae99ee0bd0a46e8bfa361
cheeyan648 <wongcy-wm24@student.tarc.edu.my>
2025-12-22 21:38:32 +0800

    44

--- _foot.php
+++ _foot.php
@@ -1,5 +1,23 @@
 </main>
-    <link rel="stylesheet" href="/css/head.css">
+
+<style>
+    footer {
+    background: #1f2937;
+    padding: 20px 40px;
+    margin-top: 80px;
+    }
+
+    footer .text {
+        text-align: right;
+        color: #e5e7eb;
+        font-size: 14px;
+    }
+
+    footer img {
+        vertical-align: middle;
+    }
+</style>
+
 <footer>
     <p class="text"><img src="../../icon/teddy-bear.png" style="height: 30px; width: 30px;">&nbsp;&nbsp;&nbsp;&copy; 2024 Your Crispy Kaw Kaw. All rights reserved.</p>
 </footer>
